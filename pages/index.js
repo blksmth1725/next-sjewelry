4fd30c241ba36eef539db011011b0799ac1e3080
Christian Sheen <csheen1725@gmail.com>
2021-12-09 16:45:20 -0500

    Adjust MaterialUI CardeMedia height property in order to achieve uniform looking card => must use cloudinary to adjust images sizes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import NextLink from "next/link";
 import {
   Button,
   Card,
@@ -20,16 +21,19 @@ export default function Home() {
           {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  ></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      height="350"
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>$ {product.price}</Typography>
                   <Button size="small" color="primary">
--- utils/data.js
+++ utils/data.js
@@ -2,6 +2,7 @@ const data = {
   products: [
     {
       name: "Ring 1",
+      slug: "ring1",
       category: "rings",
       image: "/images/ring1.jpg",
       price: 2500,
@@ -12,6 +13,7 @@ const data = {
     },
     {
       name: "Ring 2",
+      slug: "ring2",
       category: "rings",
       image: "/images/ring2.jpg",
       price: 2500,
@@ -22,6 +24,7 @@ const data = {
     },
     {
       name: "Ring 3",
+      slug: "ring3",
       category: "rings",
       image: "/images/ring3.jpg",
       price: 2500,
@@ -32,6 +35,7 @@ const data = {
     },
     {
       name: "Ring 4",
+      slug: "ring4",
       category: "rings",
       image: "/images/ring4.jpg",
       price: 2500,
@@ -42,6 +46,7 @@ const data = {
     },
     {
       name: "Ring 5",
+      slug: "ring5",
       category: "rings",
       image: "/images/ring5.jpg",
       price: 2500,
@@ -52,6 +57,7 @@ const data = {
     },
     {
       name: "Ring 6",
+      slug: "ring6",
       category: "rings",
       image: "/images/ring6.jpg",
       price: 2500,
@@ -62,6 +68,7 @@ const data = {
     },
     {
       name: "Ring 7",
+      slug: "ring7",
       category: "rings",
       image: "/images/ring7.jpg",
       price: 2500,
@@ -72,6 +79,7 @@ const data = {
     },
     {
       name: "Necklace 1",
+      slug: "necklace1",
       category: "necklaces",
       image: "/images/necklace1.jpg",
       price: 3700,
@@ -82,6 +90,7 @@ const data = {
     },
     {
       name: "Necklace 2",
+      slug: "necklace2",
       category: "necklaces",
       image: "/images/necklace2.jpg",
       price: 3700,
@@ -92,6 +101,7 @@ const data = {
     },
     {
       name: "Necklace 3",
+      slug: "necklace3",
       category: "necklaces",
       image: "/images/necklace3.jpg",
       price: 3700,
@@ -102,6 +112,7 @@ const data = {
     },
     {
       name: "Necklace 4",
+      slug: "necklace4",
       category: "necklaces",
       image: "/images/necklace4.jpg",
       price: 3700,
@@ -112,6 +123,7 @@ const data = {
     },
     {
       name: "Necklace 5",
+      slug: "necklace5",
       category: "necklaces",
       image: "/images/necklace5.jpg",
       price: 3700,
@@ -122,6 +134,7 @@ const data = {
     },
     {
       name: "Earrings 1",
+      slug: "earrings1",
       category: "earrings",
       image: "/images/earrings1.jpg",
       price: 700,
@@ -132,6 +145,7 @@ const data = {
     },
     {
       name: "Earrings 2",
+      slug: "earrings2",
       category: "earrings",
       image: "/images/earrings2.jpg",
       price: 700,
@@ -142,6 +156,7 @@ const data = {
     },
     {
       name: "Earrings 3",
+      slug: "earrings3",
       category: "earrings",
       image: "/images/earrings3.jpg",
       price: 700,
@@ -152,6 +167,7 @@ const data = {
     },
     {
       name: "Earrings 4",
+      slug: "earrings4",
       category: "earrings",
       image: "/images/earrings4.jpg",
       price: 700,
@@ -162,6 +178,7 @@ const data = {
     },
     {
       name: "Earrings 5",
+      slug: "earrings5",
       category: "earrings",
       image: "/images/earrings5.jpg",
       price: 700,
@@ -172,6 +189,7 @@ const data = {
     },
     {
       name: "Earrings 6",
+      slug: "earrings6",
       category: "earrings",
       image: "/images/earrings6.jpg",
       price: 700,
@@ -182,6 +200,7 @@ const data = {
     },
     {
       name: "Earrings 7",
+      slug: "earrings7",
       category: "earrings",
       image: "/images/earrings7.jpg",
       price: 700,
@@ -192,6 +211,7 @@ const data = {
     },
     {
       name: "Bracelet 1",
+      slug: "bracelet1",
       category: "bracelets",
       image: "/images/bracelet1.jpg",
       price: 500,
@@ -202,6 +222,7 @@ const data = {
     },
     {
       name: "Bracelet 2",
+      slug: "bracelet2",
       category: "bracelets",
       image: "/images/bracelet2.jpg",
       price: 500,
@@ -212,6 +233,7 @@ const data = {
     },
     {
       name: "Bracelet 3",
+      slug: "bracelet3",
       category: "bracelets",
       image: "/images/bracelet3.jpg",
       price: 500,
@@ -222,6 +244,7 @@ const data = {
     },
     {
       name: "Bracelet 4",
+      slug: "bracelet4",
       category: "bracelets",
       image: "/images/bracelet4.jpg",
       price: 500,
@@ -232,6 +255,7 @@ const data = {
     },
     {
       name: "Bracelet 5",
+      slug: "bracelet5",
       category: "bracelets",
       image: "/images/bracelet5.jpg",
       price: 500,
@@ -242,6 +266,7 @@ const data = {
     },
     {
       name: "Bracelet 6",
+      slug: "bracelet6",
       category: "bracelets",
       image: "/images/bracelet6.jpg",
       price: 500,
@@ -252,6 +277,7 @@ const data = {
     },
     {
       name: "Bracelet 7",
+      slug: "bracelet7",
       category: "bracelets",
       image: "/images/bracelet7.jpg",
       price: 500,
@@ -262,6 +288,7 @@ const data = {
     },
     {
       name: "Bracelet 8",
+      slug: "bracelet8",
       category: "bracelets",
       image: "/images/bracelet8.jpg",
       price: 500,
@@ -272,6 +299,7 @@ const data = {
     },
     {
       name: "Bracelet 9",
+      slug: "bracelet9",
       category: "bracelets",
       image: "/images/bracelet9.jpg",
       price: 500,
@@ -282,6 +310,7 @@ const data = {
     },
     {
       name: "Bracelet 10",
+      slug: "bracelet10",
       category: "bracelets",
       image: "/images/bracelet10.jpg",
       price: 500,
@@ -292,6 +321,7 @@ const data = {
     },
     {
       name: "Watch 1",
+      slug: "watch1",
       category: "watches",
       image: "/images/watch1.jpg",
       price: 1000,
@@ -302,6 +332,7 @@ const data = {
     },
     {
       name: "Watch 2",
+      slug: "watch2",
       category: "watches",
       image: "/images/watch2.jpg",
       price: 1000,
@@ -312,6 +343,7 @@ const data = {
     },
     {
       name: "Watch 3",
+      slug: "watch3",
       category: "watches",
       image: "/images/watch3.jpg",
       price: 1000,
@@ -322,6 +354,7 @@ const data = {
     },
     {
       name: "Watch 4",
+      slug: "watch4",
       category: "watches",
       image: "/images/watch4.jpg",
       price: 1000,
@@ -332,6 +365,7 @@ const data = {
     },
     {
       name: "Watch 5",
+      slug: "watch5",
       category: "watches",
       image: "/images/watch5.jpg",
       price: 1000,
@@ -342,6 +376,7 @@ const data = {
     },
     {
       name: "Watch 6",
+      slug: "watch6",
       category: "watches",
       image: "/images/watch6.jpg",
       price: 1000,
@@ -352,6 +387,7 @@ const data = {
     },
     {
       name: "Watch 7",
+      slug: "watch7",
       category: "watches",
       image: "/images/watch7.jpg",
       price: 1000,
@@ -362,6 +398,7 @@ const data = {
     },
     {
       name: "Cuffling 1",
+      slug: "cuffling1",
       category: "cufflings",
       image: "/images/cufflings1.jpg",
       price: 200,
@@ -372,6 +409,7 @@ const data = {
     },
     {
       name: "Cuffling 2",
+      slug: "cuffling2",
       category: "cufflings",
       image: "/images/cufflings2.jpg",
       price: 200,
@@ -382,6 +420,7 @@ const data = {
     },
     {
       name: "Cuffling 3",
+      slug: "cuffling3",
       category: "cufflings",
       image: "/images/cufflings3.jpg",
       price: 200,
@@ -392,6 +431,7 @@ const data = {
     },
     {
       name: "Cuffling 4",
+      slug: "cuffling4",
       category: "cufflings",
       image: "/images/cufflings4.jpg",
       price: 200,
@@ -402,6 +442,7 @@ const data = {
     },
     {
       name: "Cuffling 5",
+      slug: "cuffling5",
       category: "cufflings",
       image: "/images/cufflings5.jpg",
       price: 200,
