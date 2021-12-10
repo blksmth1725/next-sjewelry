60f1690714f35ab66321ccb4fa7753f36852d727
Christian Sheen <csheen1725@gmail.com>
2021-12-10 18:24:42 -0500

    Update ProductScreen.js to display name of product in "h1" tag once onClick product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useRouter } from "next/router";
+import data from "../../utils/data";
 
 const ProductScreen = () => {
-  return <div></div>;
+  const router = useRouter();
+  const { slug } = router.query;
+  const product = data.products.find((a) => a.slug === slug);
+
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
+
+  return <h1>{product.name}</h1>;
 };
 
 export default ProductScreen;
